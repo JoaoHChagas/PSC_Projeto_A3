38a29a39e63a6f1816312a548692e69ea1cf42a6
Ezequiel <luz.zeque@gmail.com>
2025-06-03 00:43:29 -0300

    Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -2,30 +2,46 @@
 package pessoa;
 
 public class Aluno extends Pessoa {
+    
     private int faltas;
-    private int totalAulas;
+    private int presencas;
+    private double getTotalAulas;
     
-    public Aluno(String nome, int totalAulas){
+    public Aluno(String nome, int presencas, int faltas ){
         super(nome);
-        
-        this.totalAulas = totalAulas;
-        this.faltas = 0;
+        this.presencas = presencas;
+        this.faltas = faltas;
     }
-    
+
+    public void setPresencas(int presencas) {
+        this.presencas = presencas;
+    }
     public void setFaltas(int faltas){
     this.faltas = faltas;
     }
     public int getFaltas(){
         return faltas; 
     }
+    
+    public int getTotalAulas(){
+        return presencas + faltas;
+    }
     public int getPresencas() {
-        return totalAulas - faltas; 
+        return presencas; 
     }
-    
+ 
     public double getPorcentagemPresenca(){
-        return(getPresencas() * 100.0) / totalAulas;
+        int total = presencas + faltas;
+        return total == 0  ? 0 : (faltas * 100.0) / total;
     }
     
+    public double getPorcentagemFaltas(){
+        int total = presencas + faltas;
+        return total == 0 ? 0 : (faltas * 100.0) / total;
+    }
+    
+}
+
     public double getPorcentagemFaltas(){
         return (faltas * 100.0) / totalAulas;
     }
